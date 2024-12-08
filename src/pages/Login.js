2af021fe777e34d1b355c71b7fe923509028647f
Ahmet Rauf OKTAY <68822176+ahmetraufoktay@users.noreply.github.com>
2024-12-08 18:11:16 +0300

    Fix: Change useState to useRef to prevent unnecessary rendering Add setLoading to prevent registering multiple users as a client side prevention

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,59 +1,74 @@
-import React, { useState } from 'react';
-import { loginUser } from '../services/usersService';
+import { useRef, useState } from "react";
+import { loginUser } from "../services/usersService";
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
-  const [success, setSuccess] = useState('');
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     // Boş alan kontrolü
-    if (!email || !password) {
-      setError('Lütfen tüm alanları doldurunuz.');
+    if (!emailRef.current.value || !passwordRef.current.value) {
+      setError("Lütfen tüm alanları doldurunuz.");
       return;
     }
 
-    const credentials = { email, password };
+    const credentials = {
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+    };
 
     try {
+      setLoading(
+        true
+      ); /* API Çalışırken tekrar butona basılmasını engellemek için loading state ini devreye sokuyoruz */
       const response = await loginUser(credentials);
       if (response.isSuccess) {
-        setSuccess('Giriş başarılı! Hoş geldiniz.');
-        setError('');
+        setSuccess("Giriş başarılı! Hoş geldiniz.");
+        setError("");
       } else {
-        setError('Giriş başarısız. Lütfen bilgilerinizi kontrol edin.');
+        setError("Giriş başarısız. Lütfen bilgilerinizi kontrol edin.");
       }
     } catch (err) {
-      setError('Giriş başarısız. Lütfen tekrar deneyin.');
-      setSuccess('');
+      setError("Giriş başarısız. Lütfen tekrar deneyin.");
+      setSuccess("");
+    } finally {
+      setLoading(false); /* Register olunca engelleme işlemini bitiriyoruz */
     }
   };
 
   return (
     <div className="flex items-center justify-center">
       <div className="bg-[#1a1a1a] p-8 rounded-lg w-full max-w-lg">
-        <h2 className="text-[#eadaff] text-3xl font-bold mb-6 text-center">Giriş Yap</h2>
+        <h2 className="text-[#eadaff] text-3xl font-bold mb-6 text-center">
+          Giriş Yap
+        </h2>
 
         {/* Form yapısına onSubmit ekleniyor */}
-        <form onSubmit={handleSubmit} className="h-96 overflow-y-auto content-center">
+        <form
+          onSubmit={handleSubmit}
+          className="h-96 overflow-y-auto content-center"
+        >
+          {/* Email Kısmı */}
           <div className="mb-4">
             <input
               type="email"
               placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              ref={emailRef}
               className="w-full p-3 border border-gray-300 bg-[#1a1a1a] text-[#eadaff] placeholder-[#807b8b] rounded focus:outline-none focus:ring-2 focus:ring-[#eadaff]"
             />
           </div>
+          {/* Şifre Kısmı */}
           <div className="mb-4">
             <input
               type="password"
               placeholder="Şifre"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              ref={passwordRef}
               className="w-full p-3 border border-gray-300 bg-[#1a1a1a] text-[#eadaff] placeholder-[#807b8b] rounded focus:outline-none focus:ring-2 focus:ring-[#eadaff]"
             />
           </div>
@@ -63,7 +78,13 @@ const Login = () => {
           {success && <p className="text-green-500 mb-4">{success}</p>}
 
           {/* Login butonu */}
-          <button type="submit" className="w-full bg-[#eadaff] text-[#1a1a1a] py-3 rounded-lg hover:bg-[#e0c9ff] transition duration-300">
+          <button
+            type="submit"
+            disabled={
+              loading
+            } /* API Çalışırken bir sürü kez basılınca register işlemini önlemek için loading state'i kuruyoruz.  */
+            className="w-full bg-[#eadaff] text-[#1a1a1a] py-3 rounded-lg hover:bg-[#e0c9ff] transition duration-300 active:scale-95"
+          >
             Giriş Yap!
           </button>
         </form>
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,19 +1,20 @@
-import React, { useState } from 'react';
-import { registerUser } from '../services/usersService';
+import React, { useState, useRef } from "react";
+import { registerUser } from "../services/usersService";
 
 const Register = () => {
-  const [name, setName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [telephoneNumber, setTelephoneNumber] = useState('');
-  const [university, setUniversity] = useState('');
-  const [department, setDepartment] = useState('');
-  const [dateOfBirth, setDateOfBirth] = useState('');
-  const [role, setRole] = useState('');
-  const [active, setActive] = useState(true);
-  const [error, setError] = useState('');
-  const [success, setSuccess] = useState('');
+  const nameRef = useRef();
+  const lastNameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const phoneRef = useRef();
+  const universityRef = useRef();
+  const departmentRef = useRef();
+  const birthRef = useRef();
+  const roleRef = useRef();
+  const activeRef = useRef();
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const convertDateToISO = (date) => {
     const dateObj = new Date(date);
@@ -24,141 +25,150 @@ const Register = () => {
     e.preventDefault();
 
     // Boş alan kontrolü
-    if (!name || !lastName || !email || !password || !telephoneNumber || !university || !department || !dateOfBirth || !role) {
-      setError('Lütfen tüm alanları doldurunuz.');
+    if (
+      !nameRef.current.value ||
+      !lastNameRef.current.value ||
+      !emailRef.current.value ||
+      !passwordRef.current.value ||
+      !phoneRef.current.value ||
+      !universityRef.current.value ||
+      !departmentRef.current.value ||
+      !birthRef.current.value ||
+      !roleRef.current.value
+    ) {
+      setError("Lütfen tüm alanları doldurunuz.");
       return;
     }
 
     const user = {
-      name,
-      last_name: lastName,
-      email,
-      password,
-      telephone_number: telephoneNumber,
-      university,
-      department,
-      date_of_birth: convertDateToISO(dateOfBirth),
-      role,
-      active,
+      name: nameRef.current.value,
+      last_name: lastNameRef.current.value,
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+      telephone_number: phoneRef.current.value,
+      university: universityRef.current.value,
+      department: departmentRef.current.value,
+      date_of_birth: convertDateToISO(birthRef.current.value),
+      role: roleRef.current.value,
+      active: activeRef.current.value,
     };
 
     try {
+      setLoading(
+        true
+      ); /* API Çalışırken tekrar butona basılmasını engellemek için loading state ini devreye sokuyoruz */
       const response = await registerUser(user);
 
       if (response.isSuccess) {
-        setSuccess('Kayıt başarılı! Şimdi giriş yapabilirsiniz.');
-        setError('');
+        console.log(convertDateToISO(birthRef.current.value));
+        setSuccess("Kayıt başarılı! Şimdi giriş yapabilirsiniz.");
+        setError("");
       } else {
-        setError('Kayıt başarısız. Lütfen bilgilerinizi kontrol edin.');
+        setError("Kayıt başarısız. Lütfen bilgilerinizi kontrol edin.");
       }
     } catch (err) {
-      setError('Kayıt başarısız. Lütfen tekrar deneyin.');
-      setSuccess('');
+      setError("Kayıt başarısız. Lütfen tekrar deneyin.");
+      setSuccess("");
+    } finally {
+      setLoading(false); /* Register olunca engelleme işlemini bitiriyoruz */
     }
   };
 
   return (
     <div className="flex items-center justify-center">
       <div className="bg-[#1a1a1a] p-8 rounded-lg w-full max-w-lg">
-        <h2 className="text-[#eadaff] text-3xl font-bold mb-6 text-center">Kayıt Ol</h2>
+        <h2 className="text-[#eadaff] text-3xl font-bold mb-6 text-center">
+          Kayıt Ol
+        </h2>
 
         {/* Form yapısı onSubmit ile düzenleniyor */}
         <form onSubmit={handleSubmit} className="h-96 overflow-y-auto pr-1">
+          {/* Ad */}
           <div className="mb-4">
             <input
               type="text"
               placeholder="İsim"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
+              ref={nameRef}
               className="w-full p-3 border border-gray-300 bg-[#1a1a1a] text-[#eadaff] placeholder-[#807b8b] rounded focus:outline-none focus:ring-2 focus:ring-[#eadaff]"
             />
           </div>
-
+          {/* Soyad */}
           <div className="mb-4">
             <input
               type="text"
               placeholder="Soyisim"
-              value={lastName}
-              onChange={(e) => setLastName(e.target.value)}
+              ref={lastNameRef}
               className="w-full p-3 border border-gray-300 bg-[#1a1a1a] text-[#eadaff] placeholder-[#807b8b] rounded focus:outline-none focus:ring-2 focus:ring-[#eadaff]"
             />
           </div>
-
+          {/* Email Adresi */}
           <div className="mb-4">
             <input
               type="email"
               placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              ref={emailRef}
               className="w-full p-3 border border-gray-300 bg-[#1a1a1a] text-[#eadaff] placeholder-[#807b8b] rounded focus:outline-none focus:ring-2 focus:ring-[#eadaff]"
             />
           </div>
-
+          {/* Şifre */}
           <div className="mb-4">
             <input
               type="password"
               placeholder="Şifre"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              ref={passwordRef}
               className="w-full p-3 border border-gray-300 bg-[#1a1a1a] text-[#eadaff] placeholder-[#807b8b] rounded focus:outline-none focus:ring-2 focus:ring-[#eadaff]"
             />
           </div>
-
+          {/* Telefon Numarası */}
           <div className="mb-4">
             <input
               type="text"
               placeholder="Telefon Numarası"
-              value={telephoneNumber}
-              onChange={(e) => setTelephoneNumber(e.target.value)}
+              ref={phoneRef}
               className="w-full p-3 border border-gray-300 bg-[#1a1a1a] text-[#eadaff] placeholder-[#807b8b] rounded focus:outline-none focus:ring-2 focus:ring-[#eadaff]"
             />
           </div>
-
+          {/* Üniversite */}
           <div className="mb-4">
             <input
               type="text"
               placeholder="Üniversite"
-              value={university}
-              onChange={(e) => setUniversity(e.target.value)}
+              ref={universityRef}
               className="w-full p-3 border border-gray-300 bg-[#1a1a1a] text-[#eadaff] placeholder-[#807b8b] rounded focus:outline-none focus:ring-2 focus:ring-[#eadaff]"
             />
           </div>
-
+          {/* Bölüm */}
           <div className="mb-4">
             <input
               type="text"
               placeholder="Bölüm"
-              value={department}
-              onChange={(e) => setDepartment(e.target.value)}
+              ref={departmentRef}
               className="w-full p-3 border border-gray-300 bg-[#1a1a1a] text-[#eadaff] placeholder-[#807b8b] rounded focus:outline-none focus:ring-2 focus:ring-[#eadaff]"
             />
           </div>
-
+          {/* Doğum Tarihi */}
           <div className="mb-4">
             <input
               type="date"
-              value={dateOfBirth}
-              onChange={(e) => setDateOfBirth(e.target.value)}
+              ref={birthRef}
               className="w-full p-3 border border-gray-300 bg-[#1a1a1a] text-[#eadaff] placeholder-[#807b8b] rounded focus:outline-none focus:ring-2 focus:ring-[#eadaff]"
             />
           </div>
-
+          {/* Kulüp Rolü  */}
           <div className="mb-4">
             <input
               type="text"
               placeholder="Rol (Örn: lead)"
-              value={role}
-              onChange={(e) => setRole(e.target.value)}
+              ref={roleRef}
               className="w-full p-3 border border-gray-300 bg-[#1a1a1a] text-[#eadaff] placeholder-[#807b8b] rounded focus:outline-none focus:ring-2 focus:ring-[#eadaff]"
             />
           </div>
-
+          {/* Aktif Kullanıcılık */}
           <div className="mb-6">
             <label className="inline-flex items-center text-[#eadaff]">
               <input
                 type="checkbox"
-                checked={active}
-                onChange={(e) => setActive(e.target.checked)}
+                ref={activeRef}
                 className="form-checkbox h-5 w-5 text-[#eadaff]"
               />
               <span className="ml-2">Aktif Kullanıcı</span>
@@ -170,7 +180,13 @@ const Register = () => {
           {success && <p className="text-green-500 mb-4">{success}</p>}
 
           {/* Kayıt butonu */}
-          <button type="submit" className="w-full bg-[#eadaff] text-[#1a1a1a] py-3 rounded-lg hover:bg-[#e0c9ff] transition duration-300">
+          <button
+            type="submit"
+            disabled={
+              loading
+            } /* API Çalışırken bir sürü kez basılınca register işlemini önlemek için loading state'i kuruyoruz.  */
+            className="w-full bg-[#eadaff] text-[#1a1a1a] py-3 rounded-lg hover:bg-[#e0c9ff] transition duration-300 active:scale-95"
+          >
             Kayıt Ol!
           </button>
         </form>
